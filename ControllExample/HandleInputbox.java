b6d45a29c92bd6e6a1698e96b71c6a0428d7e164
HimaIBM <148639617+HimaIBM@users.noreply.github.com>
2023-10-26 06:59:51 -0700

    Added new fields

---
+++ ControllExample/HandleInputbox.java
@@ -0,0 +1,21 @@
+package ControllExample;
+
+	import org.junit.Test;
+	import org.openqa.selenium.By;
+
+	public class HandleInputbox extends BaseTest{
+	    
+	    
+	    @Test
+	    public void InputBoxExample() {
+	        
+	        
+	        driver.findElement(By.xpath("//input[@name=\"fname\"]")).sendKeys("enter first Name");
+//	      driver.findElement(By.xpath("//input[@name='fname']")).sendKeys("enter first Name");
+	        
+	        
+	    }
+
+	}
+
+
